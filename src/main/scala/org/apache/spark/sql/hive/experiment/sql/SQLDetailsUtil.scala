77b12d9e010dc19cc2dc4bf066becdc6d54de1c0
Sharad Singh <sharadsingh8@gmail.com>
2024-12-09 21:03:31 +0530

    handled more case
    handled more case with unresolved star

--- src/main/scala/org/apache/spark/sql/hive/experiment/sql/SQLDetailsUtil.scala
+++ src/main/scala/org/apache/spark/sql/hive/experiment/sql/SQLDetailsUtil.scala
@@ -2,10 +2,45 @@ package org.apache.spark.sql.hive.experiment.sql
 
 object SQLDetailsUtil {
 
-  abstract class PlanDetails()
+  abstract class PlanDetails() {
+    def getRelationalDetails: Seq[QualifiedColumn]
+  }
 
-  case class RelationDetails(dbName: String, tableName: String) extends PlanDetails
+
+  case class RelationDetails(dbName: String, tableName: String) extends PlanDetails {
+    override def getRelationalDetails: Seq[QualifiedColumn] = {
+      Seq(QualifiedColumn(dbName, tableName, "all*c"))
+    }
+  }
 
   case class QualifiedColumn(dbName: String, tableName: String, columnName: String)
-  case class InterimPlanDetails(optype: String, attributes: Seq[QualifiedColumn], expressions: Seq[String], lineageInfo: Option[Map[String, String]]=None) extends PlanDetails
+
+  case class InterimPlanDetails(optype: String, attributes: Seq[QualifiedColumn],
+                                expressions: Seq[String], lineageInfo: Option[Map[String, String]] = None)
+    extends PlanDetails {
+    override def getRelationalDetails: Seq[QualifiedColumn] = {
+      attributes
+    }
+  }
+
+  case class QualifiedColumns(dbName: String, tableName: String, columnNames: Seq[String])
+
+
+  def getQualifiedColumns(inputResult: Seq[QualifiedColumn]): Seq[QualifiedColumns] = {
+    inputResult.
+      map(r => ((r.dbName, r.tableName), r.columnName)).
+      groupBy(f => f._1).map(mp => {
+        val dbName = mp._1._1
+        val tableName = mp._1._2
+        val columns = mp._2.map(x => x._2).distinct
+        val normalizedColumns = if(columns.length>1){
+          columns.filter(r=> !r.equalsIgnoreCase("all*c"))
+        }else{
+          columns
+        }
+        QualifiedColumns(dbName, tableName, normalizedColumns)
+      }
+      ).toSeq
+  }
 }
+
