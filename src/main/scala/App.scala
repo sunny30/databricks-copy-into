0db18f8ffe6e3b53d70a3329953e3f68908bcdee
Sharad Singh <sharadsingh8@gmail.com>
2025-01-12 03:17:27 +0530

    testing with merge
    testing with merge

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -174,43 +174,64 @@ object App {
     //   df2.write.format("delta").saveAsTable("cat.dbx116.ttd")
 
     /****-----delta merge update and delete table issue ------****/
-    spark.sql("create database cat.dbx116")
-//
-    spark.sql("CREATE TABLE cat.dbx116.ttd (col1 String) USING delta")
-    spark.sql(""" INSERT INTO cat.dbx116.ttd VALUES ('99'), ('6'), ('7')""".stripMargin)
-    df2.write.insertInto("cat.dbx116.ttd")
-    spark.sql("select * from cat.dbx116.ttd").show()
-    spark.read.table("cat.dbx116.ttd").show()
-    spark.sql("update cat.dbx116.ttd set col1 = 'six' where col1 = '6' ").show()
-    spark.sql("select col1 from cat.dbx116.ttd").show()
+//    spark.sql("create database cat.dbx116")
+////
+//    spark.sql("CREATE TABLE cat.dbx116.ttd (col1 String) USING delta")
+//    spark.sql(""" INSERT INTO cat.dbx116.ttd VALUES ('99'), ('6'), ('7')""".stripMargin)
+//    df2.write.insertInto("cat.dbx116.ttd")
 //    spark.sql("select * from cat.dbx116.ttd").show()
-    spark.sql("delete from cat.dbx116.ttd where col1 = '99'")
-    spark.read.table("cat.dbx116.ttd").show()
-    spark.sql("describe history cat.dbx116.ttd").show
-    spark.sql("describe detail cat.dbx116.ttd").show
-
-     //   spark.read.table("cat.dbx116.ttd").show()
- //   spark.sql("select * from cat.dbx116.ttd").show()
-
-    //
-    //    df2.write.insertInto("cat.dbx113.ttp")
-    //    spark.read.table("cat.dbx113.ttp").show()
-    //
-    /**---normal data source table operation ----**/
-
-
-    spark.sql("create database cat.dbx119")
-    spark.sql("create table cat.dbx119.ttp(c1 int, c2 int) using parquet")
-    spark.sql("""insert into cat.dbx119.ttp values (1,11), (2,22), (3,33)""")
-    spark.sql("select * from cat.dbx119.ttp where c1>1").show
-
-    spark.sql("create view cat.dbx119.v(id, id11) as select * from  cat.dbx119.ttp")
-    spark.sql("select * from cat.dbx119.v").show()
-    spark.sql("select c1, c2, fadd('', c1, c2) as c3 from cat.dbx119.ttp").show
-    spark.sql("""create table cat.dbx119.ttd(c1 int, c2 int) using delta cluster by(c1)""")
+//    spark.read.table("cat.dbx116.ttd").show()
+//    spark.sql("update cat.dbx116.ttd set col1 = 'six' where col1 = '6' ").show()
+//    spark.sql("select col1 from cat.dbx116.ttd").show()
+////    spark.sql("select * from cat.dbx116.ttd").show()
+//    spark.sql("delete from cat.dbx116.ttd where col1 = '99'")
+//    spark.read.table("cat.dbx116.ttd").show()
+//    spark.sql("describe history cat.dbx116.ttd").show
+//    spark.sql("describe detail cat.dbx116.ttd").show
+//
+//     //   spark.read.table("cat.dbx116.ttd").show()
+// //   spark.sql("select * from cat.dbx116.ttd").show()
+//
+//    //
+//    //    df2.write.insertInto("cat.dbx113.ttp")
+//    //    spark.read.table("cat.dbx113.ttp").show()
+//    //
+//    /**---normal data source table operation ----**/
+//
+//
+//    spark.sql("create database cat.dbx119")
+//    spark.sql("create table cat.dbx119.ttp(c1 int, c2 int) using parquet")
+//    spark.sql("""insert into cat.dbx119.ttp values (1,11), (2,22), (3,33)""")
+//    spark.sql("select * from cat.dbx119.ttp where c1>1").show
+//
+//    spark.sql("create view cat.dbx119.v(id, id11) as select * from  cat.dbx119.ttp")
+//    spark.sql("select * from cat.dbx119.v").show()
+//    spark.sql("select c1, c2, fadd('', c1, c2) as c3 from cat.dbx119.ttp").show
+//    spark.sql("""create table cat.dbx119.ttd(c1 int, c2 int) using delta cluster by(c1)""")
 
 
     /**---end of normal data source table operation ----**/
+
+    /*delta merge*/
+    spark.sql("create database cat.dbx120")
+    spark.sql("create table cat.dbx120.ttp(c1 int, c2 int) using parquet")
+    spark.sql("""insert into cat.dbx120.ttp values (1,11), (2,22), (3,33)""")
+    spark.sql("create table cat.dbx120.ttd(c1 int, c2 int) using delta")
+    spark.sql("""insert into cat.dbx120.ttd values (1,111), (4,44), (33,33)""")
+    spark.sql(
+      """merge into cat.dbx120.ttd using cat.dbx120.ttp on cat.dbx120.ttd.c1 = cat.dbx120.ttp.c1
+        |WHEN MATCHED THEN UPDATE SET
+        |c1 = cat.dbx120.ttp.c1,
+        |c2 = cat.dbx120.ttp.c2
+        |WHEN NOT MATCHED
+        |  THEN INSERT (
+        |  c1,
+        |  c2)
+        |  values(
+        |  cat.dbx120.ttp.c1,
+        |  cat.dbx120.ttp.c2)""".stripMargin)
+    spark.read.table("cat.dbx120.ttd").show()
+    spark.sql("describe history cat.dbx120.ttd").show()
 
 
 
