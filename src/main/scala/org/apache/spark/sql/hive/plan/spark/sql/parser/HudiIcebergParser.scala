35b345b69921d47beb77063efd6b4d5669700b5b
Sharad Singh <sharadsingh8@gmail.com>
2025-06-30 18:29:36 +0530

    bug fix
    bug fix

--- src/main/scala/org/apache/spark/sql/hive/plan/spark/sql/parser/HudiIcebergParser.scala
+++ src/main/scala/org/apache/spark/sql/hive/plan/spark/sql/parser/HudiIcebergParser.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.catalyst.parser.ParserInterface
 import org.apache.spark.sql.catalyst.parser.extensions.IcebergSparkSqlExtensionsParser
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.hudi.parser.HoodieSpark2ExtendedSqlParser
+import org.apache.spark.sql.parser.HoodieCommonSqlParser
 
 class HudiIcebergParser(val delegate: ParserInterface) {
 
@@ -14,7 +15,7 @@ class HudiIcebergParser(val delegate: ParserInterface) {
 
     val icebergParser = new IcebergSparkSqlExtensionsParser(delegate)
     val conf = SparkSession.active.sqlContext.conf
-    val hudiParser = new HoodieSpark2ExtendedSqlParser(SparkSession.active, delegate)
+    val hudiParser = new HoodieCommonSqlParser(SparkSession.active, delegate)
 
     try{
       icebergParser.parsePlan(sqlText)
