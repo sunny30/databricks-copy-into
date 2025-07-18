8a3c406aad9d7468792aaf9e5903104cf96e1d09
Sharad Singh <sharadsingh8@gmail.com>
2025-07-18 19:21:10 +0530

    CatalogStatistics patched
    CatalogStatistics patched

--- src/main/scala/org/apache/spark/sql/connector/catalog/TableSchemaChangeCatalog.scala
+++ src/main/scala/org/apache/spark/sql/connector/catalog/TableSchemaChangeCatalog.scala
@@ -1,17 +1,32 @@
 package org.apache.spark.sql.connector.catalog;
 
-import org.apache.iceberg.types.Types;
-import org.apache.spark.sql.catalyst.analysis.NoSuchTableException;
+import org.apache.iceberg.types.Types
+import org.apache.spark.sql.catalyst.analysis.NoSuchTableException
+import org.apache.spark.sql.catalyst.catalog.CatalogStatistics
 import org.apache.spark.sql.types.StructType;
 
 trait TableSchemaChangeCatalog extends CatalogPlugin {
 
-    def alterTable(
-             ident:Identifier,
-             schema:StructType):Unit={}
+  def alterTable(
+                  ident: Identifier,
+                  schema: StructType): Unit = {}
 
 
-    def getTableLocation(db:String,
-                         table: String
-                        ):String
+  def getTableLocation(db: String,
+                       table: String
+                      ): String
+
+
+  def alterTableStats(
+                       db: String,
+                       table: String,
+                       stats: Option[CatalogStatistics]
+                     ):Unit
+
+  def getTableStats(
+                   db: String,
+                   table: String
+                   ): Option[CatalogStatistics]
+
+
 }
