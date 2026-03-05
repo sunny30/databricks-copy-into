8f605516d366adec79af79dc5176e180627325ce
Sharad Singh <sharadsingh8@gmail.com>
2026-03-06 04:31:53 +0530

    code changes for datatype parsing

--- src/main/scala/VanilaSparkApp.scala
+++ src/main/scala/VanilaSparkApp.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StructType
 
 object VanilaSparkApp {
 
@@ -20,7 +21,51 @@ object VanilaSparkApp {
 //    spark.sql("create table vt1(id int) using delta")
 //    spark.sql("insert into vt1 values(1), (2)")
 //    spark.sql("select * from vt1 as of version 0")
-    spark.read.format("avro").load("/Users/sharadsingh/Dev/databricks-copy-into/spark-warehouse/cat.cat/lidb.db/ptbl_BACKUP_/metadata/1609def0-56c8-4b44-83da-a5245ed9dbbf-m0.avro").show(truncate = false)
+   // spark.read.format("avro").load("/Users/sharadsingh/Dev/databricks-copy-into/spark-warehouse/cat.cat/lidb.db/ptbl_BACKUP_/metadata/1609def0-56c8-4b44-83da-a5245ed9dbbf-m0.avro").show(truncate = false)
+
+    val dt = spark.sessionState.sqlParser.parseDataType("""STRUCT<
+                                                 |    `1%`: DOUBLE,
+                                                 |    `5%`: DOUBLE,
+                                                 |    `10%`: DOUBLE,
+                                                 |    `25%`: DOUBLE,
+                                                 |    `50%`: DOUBLE,
+                                                 |    `75%`: DOUBLE,
+                                                 |    `90%`: DOUBLE,
+                                                 |    `95%`: DOUBLE,
+                                                 |    `99%`: DOUBLE,
+                                                 |    `count`: DOUBLE,
+                                                 |    `max`: DOUBLE,
+                                                 |    `mean`: DOUBLE,
+                                                 |    `min`: DOUBLE,
+                                                 |    `std`: DOUBLE
+                                                 |  >""".stripMargin)
+    val s = """STRUCT<
+                 |    1%: DOUBLE,
+                 |    5%: DOUBLE,
+                 |    10%: DOUBLE,
+                 |    `25%`: DOUBLE,
+                 |    `50%`: DOUBLE,
+                 |    `75%`: DOUBLE,
+                 |    `90%`: DOUBLE,
+                 |    `95%`: DOUBLE,
+                 |    `99%`: DOUBLE,
+                 |    `count`: DOUBLE,
+                 |    `max`: DOUBLE,
+                 |    `mean`: DOUBLE,
+                 |    `min`: DOUBLE,
+                 |    `std`: DOUBLE
+                 |  >""".stripMargin
+
+
+
+    val quotedStruct = s.replaceAll("""([^`,<\s]+)(?=\s*:)""", "`$1`")
+    val dt1 = spark.sessionState.sqlParser.parseDataType(quotedStruct)
+    println(dt1.sql)
+
+
+    // val schema = StructType.fromDDL(s)
+   // println(schema.prettyJson)
+    println(dt.sql)
     //spark.read.format("avro").load("/Users/sharadsingh/Dev/databricks-copy-into/spark-warehouse/local/db/log_orc_snap/metadata/8a82f4c6-fe2b-494c-b107-b065ee08313e-m0.avro").show(truncate = false)
   }
 
