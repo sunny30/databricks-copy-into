2b96e9fddcf1c98f5464a31c63714812237672d4
Sharad Singh <sharadsingh8@gmail.com>
2026-04-06 23:20:03 +0530

    code in progress
    code in progress

--- src/main/scala/org/apache/spark/sql/hive/plan/TwoToThreePartRule.scala
+++ src/main/scala/org/apache/spark/sql/hive/plan/TwoToThreePartRule.scala
@@ -2,14 +2,16 @@ package org.apache.spark.sql.hive.plan
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.catalyst.catalog.{CatalogTable, HiveTableRelation}
+import org.apache.spark.sql.catalyst.expressions.AttributeReference
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.connector.read.Statistics
 import org.apache.spark.sql.delta.util.AnalysisHelper
 import org.apache.spark.sql.execution.command.{CreateDataSourceTableAsSelectCommand, CreateDataSourceTableCommand, CreateTableCommand, DDLUtils}
-import org.apache.spark.sql.execution.datasources.SaveIntoDataSourceCommand
+import org.apache.spark.sql.execution.datasources.{InsertIntoDataSourceCommand, LogicalRelation, SaveIntoDataSourceCommand}
 import org.apache.spark.sql.hive.execution.CreateHiveTableAsSelectCommand
-import org.apache.spark.sql.sources.CreatableRelationProvider
+import org.apache.spark.sql.sources.{BaseRelation, CreatableRelationProvider}
 
 class TwoToThreePartRule(session: SparkSession)
   extends Rule[LogicalPlan] with AnalysisHelper with Logging {
@@ -21,6 +23,12 @@ class TwoToThreePartRule(session: SparkSession)
     case ch@CreateHiveTableAsSelectCommand(tableDesc: CatalogTable, query: LogicalPlan, outputColumnNames: Seq[String], mode: SaveMode) => ch
     case cdas@CreateDataSourceTableAsSelectCommand(table: CatalogTable, mode: SaveMode, query: LogicalPlan, outputColumnNames: Seq[String]) => cdas
     case dsw@SaveIntoDataSourceCommand(query: LogicalPlan, dataSource: CreatableRelationProvider, options: Map[String, String], mode: SaveMode)  => dsw
+    case inds@InsertIntoDataSourceCommand(logicalRelation: LogicalRelation, query: LogicalPlan, overwrite: Boolean)  => inds
+    case lrds@LogicalRelation(r: BaseRelation, _, table: Option[CatalogTable], false) if table.isDefined => lrds
+    case hiveTableRelation@HiveTableRelation(tableMeta: CatalogTable, dataCols: Seq[AttributeReference],
+    partitionCols: Seq[AttributeReference], tableStats: Option[Statistics],_) => hiveTableRelation
+
+
 
 
 
