6adbd04615e26d0a5f72289c1690036f9dcd577c
Sharad Singh <sharadsingh8@gmail.com>
2026-05-03 21:11:00 +0530

    querwrap data source for testing
    querwrap data source for testing

---
+++ src/main/scala/org/apache/spark/sql/hive/plan/may26hack/query/ExternalCatalogQueryRelation.scala
@@ -0,0 +1,41 @@
+package org.apache.spark.sql.hive.plan.may26hack.query
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
+import org.apache.spark.sql.sources.{BaseRelation, Filter, InsertableRelation, PrunedFilteredScan, TableScan}
+import org.apache.spark.sql.types.StructType
+
+case class ExternalCatalogQueryRelation(sqlContext: SQLContext, parameters: Map[String, String]) extends BaseRelation
+  with PrunedFilteredScan with InsertableRelation with TableScan {
+
+
+
+  private var _schema: StructType = null
+
+  def setSchema(sch: StructType) = this._schema = sch
+
+  override def schema: StructType = this._schema
+
+  override def buildScan(): RDD[Row] = {
+    val spark = SparkSession.active
+    import spark.implicits._
+    val results = Seq(("2", "hello","3")).toDF("price","greet", "id")
+    results.rdd
+
+  }
+
+  override def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] = {
+    val spark = SparkSession.active
+    import spark.implicits._
+    val results = Seq(("2", "hello", "3.0")).toDF("price", "greet", "id")
+    //    val p = results.select(requiredColumns.head, requiredColumns.tail:_*)
+    results.rdd
+  }
+
+  override def insert(data: DataFrame, overwrite: Boolean): Unit = {
+    println(s"${data.collect().toString} and flag overwrite is ${overwrite.toString}")
+    println("Inside insert")
+  }
+
+
+}
